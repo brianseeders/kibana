364ba441de40657a444d0d420c76e4d09b0fabd6
Brian Seeders <brian.seeders@elastic.co>
2019-10-29 11:48:28 -0400

    Setup windows ieSmoke job based on Dima's work

--- .ci/run.sh
+++ .ci/run.sh
@@ -11,30 +11,33 @@ source src/dev/ci_setup/setup.sh
 source src/dev/ci_setup/checkout_sibling_es.sh
 
 case "$JOB" in
-kibana-intake | windows-kibana-intake)
+kibana-intake)
   ./test/scripts/jenkins_unit.sh
   ;;
-kibana-ciGroup* | windows-kibana-ciGroup*)
+kibana-ciGroup*)
   export CI_GROUP="${JOB##kibana-ciGroup}"
   ./test/scripts/jenkins_ci_group.sh
   ;;
-kibana-visualRegression* | windows-kibana-visualRegression*)
+kibana-visualRegression*)
   ./test/scripts/jenkins_visual_regression.sh
   ;;
-kibana-firefoxSmoke* | windows-kibana-firefoxSmoke*)
+kibana-firefoxSmoke*)
   ./test/scripts/jenkins_firefox_smoke.sh
   ;;
-x-pack-intake | windows-x-pack-intake)
+windows-kibana-ieSmoke*)
+  ./test/scripts/jenkins_ie_smoke.sh
+  ;;
+x-pack-intake)
   ./test/scripts/jenkins_xpack.sh
   ;;
-x-pack-ciGroup* | windows-x-pack-ciGroup*)
+x-pack-ciGroup*)
   export CI_GROUP="${JOB##x-pack-ciGroup}"
   ./test/scripts/jenkins_xpack_ci_group.sh
   ;;
-x-pack-visualRegression* | windows-x-pack-visualRegression*)
+x-pack-visualRegression*)
   ./test/scripts/jenkins_xpack_visual_regression.sh
   ;;
-x-pack-firefoxSmoke* | windows-x-pack-firefoxSmoke*)
+x-pack-firefoxSmoke*)
   ./test/scripts/jenkins_xpack_firefox_smoke.sh
   ;;
 *)
