84dbd3127ea76bccc213e734b1fb3e81ed1a00bb
Brian Seeders <brian.seeders@elastic.co>
2019-10-29 17:31:47 -0400

    Run IE smoke from source for now

--- test/scripts/jenkins_ie_smoke.sh
+++ test/scripts/jenkins_ie_smoke.sh
@@ -8,24 +8,25 @@ else
   source src/dev/ci_setup/setup_env.sh
 fi
 
-if [[ -z "$IS_PIPELINE_JOB" ]] ; then
-  node scripts/build --debug --oss;
-  windowsBuild="$(find "$KIBANA_DIR/target" -name 'kibana-oss-*-windows-x86_64.zip')"
-  installDir="$PARENT_DIR/install"
-  mkdir -p "$installDir"
-  unzip -q "$windowsBuild" -d "$installDir"
-  installDir="$(find "$installDir" -maxdepth 1 -name 'kibana-*-SNAPSHOT-windows-x86_64')"
-else
-  installDir="$(realpath $PARENT_DIR/kibana/build/oss/kibana-*-SNAPSHOT-linux-x86_64)"
-  destDir=${installDir}-${CI_WORKER_NUMBER}
-  cp -R "$installDir" "$destDir"
+# if [[ -z "$IS_PIPELINE_JOB" ]] ; then
+#   node scripts/build --debug --oss;
+#   windowsBuild="$(find "$KIBANA_DIR/target" -name 'kibana-oss-*-windows-x86_64.zip')"
+#   installDir="$PARENT_DIR/install"
+#   mkdir -p "$installDir"
+#   unzip -q "$windowsBuild" -d "$installDir"
+#   installDir="$(find "$installDir" -maxdepth 1 -name 'kibana-*-SNAPSHOT-windows-x86_64')"
+# else
+#   installDir="$(realpath $PARENT_DIR/kibana/build/oss/kibana-*-SNAPSHOT-linux-x86_64)"
+#   destDir=${installDir}-${CI_WORKER_NUMBER}
+#   cp -R "$installDir" "$destDir"
 
-  export KIBANA_INSTALL_DIR="$destDir"
-fi
+#   export KIBANA_INSTALL_DIR="$destDir"
+# fi
+
+# --kibana-install-dir "$installDir" \
 
 checks-reporter-with-killswitch "Internet Explorer smoke test" \
   node scripts/functional_tests \
     --bail --debug \
-    --kibana-install-dir "$installDir" \
     --include-tag "ciGroup1" \
     --config test/functional/config.ie.js;
