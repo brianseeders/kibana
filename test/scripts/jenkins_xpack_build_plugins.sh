078e5a0bead9700a4e4e42f1b4336871fb33a0c2
Brian Seeders <brian.seeders@elastic.co>
2020-06-02 14:01:15 -0400

    Fix variables from copy/paste

--- test/scripts/jenkins_xpack_build_plugins.sh
+++ test/scripts/jenkins_xpack_build_plugins.sh
@@ -3,10 +3,10 @@
 source src/dev/ci_setup/setup_env.sh
 echo " -> building kibana platform plugins"
 node scripts/build_kibana_platform_plugins \
-  --scan-dir "\$XPACK_DIR/test/plugin_functional/plugins" \
-  --scan-dir "\$XPACK_DIR/test/functional_with_es_ssl/fixtures/plugins" \
-  --scan-dir "\$XPACK_DIR/test/alerting_api_integration/plugins" \
-  --scan-dir "\$XPACK_DIR/test/plugin_api_integration/plugins" \
-  --scan-dir "\$XPACK_DIR/test/plugin_api_perf/plugins" \
+  --scan-dir "$XPACK_DIR/test/plugin_functional/plugins" \
+  --scan-dir "$XPACK_DIR/test/functional_with_es_ssl/fixtures/plugins" \
+  --scan-dir "$XPACK_DIR/test/alerting_api_integration/plugins" \
+  --scan-dir "$XPACK_DIR/test/plugin_api_integration/plugins" \
+  --scan-dir "$XPACK_DIR/test/plugin_api_perf/plugins" \
   --workers 36 \
   --verbose
