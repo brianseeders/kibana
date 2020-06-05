fca824a5644f75ed0fb6bd6a1623e458a9b3b6b9
Brian Seeders <brian.seeders@elastic.co>
2020-06-05 17:30:43 -0400

    Some cleanup and docs

--- vars/withTaskQueue.groovy
+++ vars/withTaskQueue.groovy
@@ -3,10 +3,42 @@ import groovy.transform.Field
 public static @Field TASK_QUEUES = [:]
 public static @Field TASK_QUEUES_COUNTER = 0
 
+/**
+  withTaskQueue creates a queue of "tasks" (just plain closures to execute), and executes them with your desired level of concurrency.
+  This way, you can define, for example, 40 things that need to execute, then only allow 10 of them to execute at once.
+
+  Each "process" will execute in a separate, unique, empty directory.
+  If you want each process to have a bootstrapped kibana repo, check out kibanaPipeline.withCiTaskQueue
+
+  Using the queue currently requires an agent/worker.
+
+  Usage:
+
+  withTaskQueue(parallel: 10) {
+    task { print "This is a task" }
+
+    // This is the same as calling task() multiple times
+    tasks([ { print "Another task" }, { print "And another task" } ])
+
+    // Tasks can queue up subsequent tasks
+    task {
+      buildThing()
+      task { print "I depend on buildThing()" }
+    }
+  }
+
+  You can also define a setup task that each process should execute one time before executing tasks:
+  withTaskQueue(parallel: 10, setup: { sh "my-setup-scrupt.sh" }) {
+    ...
+  }
+
+*/
 def call(Map options = [:], Closure closure) {
   def config = [ parallel: 10 ] + options
   def counter = ++TASK_QUEUES_COUNTER
 
+  // We're basically abusing withEnv() to create a "scope" for all steps inside of a withTaskQueue block
+  // This way, we could have multiple task queue instances in the same pipeline
   withEnv(["TASK_QUEUE_ID=${counter}"]) {
     withTaskQueue.TASK_QUEUES[env.TASK_QUEUE_ID] = [
       tasks: [],
@@ -48,6 +80,8 @@ def call(Map options = [:], Closure closure) {
                       task.call()
                     }
                     processesExecuting--
+                    // If a task finishes, and no new tasks were queued up, and nothing else is executing
+                    // Then all of the processes should wake up and exit
                     if (processesExecuting < 1 && getTasks().isEmpty()) {
                       taskNotify()
                     }
@@ -59,7 +93,7 @@ def call(Map options = [:], Closure closure) {
                     return
                   }
 
-                  // taskNotify()
+                  // Queue is empty, no processes are executing
                   isDone = true
                 }
               }
@@ -72,6 +106,8 @@ def call(Map options = [:], Closure closure) {
   }
 }
 
+// If we sleep in a loop using Groovy code, Pipeline Steps is flooded with Sleep steps
+// So, instead, we just watch a file and `touch` it whenever something happens that could modify the queue
 def taskSleep() {
   sh(script: """#!/bin/bash
     TIMESTAMP=\$(date '+%s' -d "0 seconds ago")
@@ -87,9 +123,9 @@ def taskSleep() {
   """, label: "Waiting for new tasks...")
 }
 
-def addTask(Closure closure) {
-  getTasks() << closure
-  taskNotify()
+// Used to let the task queue processes know that either a new task has been queued up, or work is complete
+def taskNotify() {
+  sh "touch '${getTmpFile()}'"
 }
 
 def getTasks() {
@@ -100,8 +136,9 @@ def getTmpFile() {
   return withTaskQueue.TASK_QUEUES[env.TASK_QUEUE_ID].tmpFile
 }
 
-def taskNotify() {
-  sh "touch '${getTmpFile()}'"
+def addTask(Closure closure) {
+  getTasks() << closure
+  taskNotify()
 }
 
 def addTasks(List<Closure> closures) {
